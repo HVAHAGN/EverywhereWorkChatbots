53a051abc8f925a4b0f593f8b9e2c824ee56a5e3
hvahagn <vhoagamo@gmail.com>
2020-07-08 00:21:13 +0400

    add SlackClient

--- SlackWithCSharp/Program.cs
+++ SlackWithCSharp/Program.cs
@@ -10,6 +10,25 @@ namespace SlackWithCSharp
     {
         static void Main(string[] args)
         {
+            while (true)
+            {
+                Console.WriteLine("Please enter your message to send by slack");
+                string message = Console.ReadLine();
+                SlackMessage(message);
+                Console.WriteLine("Your message is sent!");
+                Console.Clear();
+            }
+       
+
+            //Console.ReadLine();
+        }
+        public static void SlackMessage(string message)
+        {
+            string urlWithAccesToken = "https://hooks.slack.com/services/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";
+            SlackClient client = new SlackClient(urlWithAccesToken);
+            client.PostMessage(text:message, channel: "#general");
+
+            //"https://{your_account}.slack.com/services/hooks/incoming-webhook?token={your_access_token}";
         }
     }
 }
---
+++ SlackWithCSharp/SlackClient.cs
@@ -0,0 +1,46 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SlackWithCSharp
+{
+    public class SlackClient
+    {
+        private readonly Uri _uri;
+        private readonly Encoding _encoding = new UTF8Encoding();
+        public SlackClient(string urlWithAccessToken)
+        {
+            _uri = new Uri(urlWithAccessToken);
+        }
+
+        public void PostMessage(string text, string userName=null, string channel = null)
+        {
+            Payload payload = new Payload()
+            {
+                Channel=channel,
+                UserName=userName,
+                Text=text
+            };
+            PostMessage(payload);
+        }
+        public void PostMessage(Payload payload)
+        {
+            string payloadJson = JsonConvert.SerializeObject(payload);
+            using (WebClient client=new WebClient())
+            {
+                NameValueCollection data = new NameValueCollection();
+                data["payload"] = payloadJson;
+                var response = client.UploadValues(_uri, "POST", data);
+                string responseText = _encoding.GetString(response);
+            }
+        }
+
+       
+
+    }
+}
