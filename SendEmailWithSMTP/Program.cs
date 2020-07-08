a9a625d12725d5aba2ca417aec6293c397968601
hvahagn <vhoagamo@gmail.com>
2020-07-08 22:17:00 +0400

    add Task EmailSendler

---
+++ SendEmailWithSMTP/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SendEmailWithSMTP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            await SendEmail();
+            
+            
+        }
+        public static async Task SendEmail(string toEmailAddress, string emailSubject, string emailMessage)
+        {
+            var message = new MailMessage();
+            message.To.Add(toEmailAddress);
+
+            message.Subject = emailSubject;
+            message.Body = emailMessage;
+
+            using (var smtpClient = new SmtpClient())
+            {
+                await smtpClient.SendMailAsync(message);
+            }
+        }
+    }
+}
